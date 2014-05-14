4780f5f689b72f1b44660cc22b2b99409a777921
Pippin Williamson <pippin@pippinsplugins.com>
2014-05-14 14:14:06 -0500

    Add custom FES field and slider to choose amount

--- includes/class-dev-fund.php
+++ includes/class-dev-fund.php
@@ -3,8 +3,118 @@
 class CGC_Markets_FES_Dev_Fund {
 	
 	public function __construct() {
+		add_action( 'fes_custom_post_button', array( $this, 'dev_fund_field_button' ) );
+		add_action( 'fes_admin_field_dev_fund', array( $this, 'dev_fund_admin_field' ), 10, 3 );
+		add_filter( 'fes_formbuilder_custom_field', array( $this, 'dev_fund_formbuilder_is_custom_field' ), 10, 2 );
+		add_action( 'fes_render_field_dev_fund', array( $this, 'dev_fund_field' ), 10, 3 );
 		add_action( 'fes_submit_submission_form_bottom', array( $this, 'save_dev_fund_status' ) );
+	
+		add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
+
 		add_filter( 'edd_settings_misc', array( $this, 'settings' ) );
+
+	}
+	/**
+	 * Register a custom FES submission form button 
+	 *
+	 * @since 2.0
+	 *
+	 * @return void
+	 */
+	function dev_fund_field_button( $title ) {
+		echo  '<button class="fes-button button" data-name="dev_fund" data-type="action" title="' . esc_attr( $title ) . '">Dev Fund</button>';
+	}
+
+	/**
+	 * Setup the custom FES form field
+	 *
+	 * @since 2.0
+	 *
+	 * @return void
+	 */
+	function dev_fund_admin_field( $field_id, $label = "", $values = array() ) {
+		if( ! isset( $values['label'] ) ) {
+			$values['label'] = 'Dev Fund';
+		}
+
+		$values['no_css']  = true;
+		$values['is_meta'] = true;
+		$values['name']    = 'dev_fund';
+		?>
+		<li class="dev_fund">
+			<?php FES_Formbuilder_Templates::legend( $values['label'] ); ?>
+			<?php FES_Formbuilder_Templates::hidden_field( "[$field_id][input_type]", 'dev_fund' ); ?>
+			<?php FES_Formbuilder_Templates::hidden_field( "[$field_id][template]", 'dev_fund' ); ?>
+			<div class="fes-form-holder">
+				<?php FES_Formbuilder_Templates::common( $field_id, 'dev_fund', false, $values ); ?>
+			</div> <!-- .fes-form-holder -->
+		</li>
+		<?php
+	}
+
+	/**
+	 * Indicate that this is a custom field
+	 *
+	 * @since 2.0
+	 *
+	 * @return bool
+	 */
+	function dev_fund_formbuilder_is_custom_field( $bool, $template_field ) {
+		if ( $bool ) {
+			return $bool;
+		} else if ( isset( $template_field['template'] ) && $template_field['template'] == 'dev_fund' ) {
+			return true;
+		} else {
+			return $bool;
+		}
+	}
+
+	/**
+	 * Render our dev fund rate field
+	 *
+	 * @since 2.0
+	 *
+	 * @return void
+	 */
+	function dev_fund_field( $attr, $post_id, $type ) {
+
+		if ( isset( $attr['required'] ) && $attr['required'] == 'yes' ) {
+			$required = apply_filters( 'fes_required_class', ' edd-required-indicator', $attr );
+		}
+
+		$yes     = has_term( 'dev-fund', 'download_tag', $post_id );
+		$amount  = get_post_meta( $post_id, 'dev_fund_amount', true );
+		$display = $yes ? '' : 'display:none;';
+		?>
+		<script>
+			jQuery(function($){
+				$('#dev_fund_yes,#dev_fund_no').change(function() {
+					$('#dev_fund_amount_wrap').toggle();
+				});
+				$('#dev_fund_slider').slider({
+					max: 100,
+					value: '<?php echo $amount; ?>',
+					change: function( event, ui ) {
+						$('#dev_fund_amount').val( ui.value );
+						$('#dev_fund_amount_wrap label span').text( ui.value );
+					}
+				});
+			});
+		</script>
+		<div class="fes-fields <?php echo sanitize_key( $attr['name']); ?>">
+			<label for="dev_fund_yes">
+				<input type="radio" id="dev_fund_yes" name="dev_fund" value="yes"<?php checked( true, $yes ); ?>/> Yes
+			</label>
+			<label for="dev_fund_no">
+				<input type="radio" id="dev_fund_no" name="dev_fund" value="no"<?php checked( false, $yes ); ?>/> No
+			</label>
+			<div id="dev_fund_amount_wrap" style="<?php echo $display; ?>">
+				<label>How much of each sale would you like to contribute? <span><?php echo absint( $amount ); ?></span>%</label>
+				<div id="dev_fund_slider"></div>
+				<input type="hidden" name="dev_fund_amount" id="dev_fund_amount" value="<?php echo esc_attr( absint( $amount ) ); ?>"/>
+			</div>
+		</div> <!-- .fes-fields -->
+		<?php
 	}
 
 	/**
@@ -17,8 +127,9 @@ class CGC_Markets_FES_Dev_Fund {
 	public function save_dev_fund_status( $post_id ) {
 
 		$dev_fund = ! empty( $_POST ['dev_fund'] ) ? sanitize_text_field( $_POST ['dev_fund'] ) : false;
+		$amount   = ! empty( $_POST['dev_fund_amount'] ) ? absint( $_POST['dev_fund_amount'] ) : 0;
 
-		if ( ! empty( $dev_fund ) && 'yes' == strtolower( $dev_fund ) ) {
+		if ( ! empty( $dev_fund ) && 'yes' == strtolower( $dev_fund )  && ! empty( $amount ) ) {
 			
 			// User has opted into the dev fund
 
@@ -41,10 +152,25 @@ class CGC_Markets_FES_Dev_Fund {
 				
 				}
 
+				$settings = get_post_meta( $post_id, '_edd_commission_settings', true );
+
+				// Add the dev fund user to the recipients
 				$recipients[] = $dev_fund_id;
-				$commission_settings = get_post_meta( $post_id, '_edd_commission_settings', true );
-				$commission_settings['user_id'] = implode( ',', $recipients );
-				update_post_meta( $post_id, '_edd_commission_settings', $commission_settings );
+				$settings['user_id'] = implode( ',', $recipients );
+
+				$rates      = array_map( 'trim', explode( ',', $settings['amount'] ) );
+				$rate_key   = array_search( $dev_fund_id, $recipients );
+				
+				if( false !== $rate_key ) {
+					$rates[ $rate_key ] = $amount;
+				} else {
+					$rates[] = $amount;
+				}
+
+				$settings['amount'] = implode( ',', $rates );
+
+				update_post_meta( $post_id, 'dev_fund_amount', $amount );
+				update_post_meta( $post_id, '_edd_commission_settings', $settings );
 
 			}
 
@@ -67,10 +193,26 @@ class CGC_Markets_FES_Dev_Fund {
 				
 				}
 
+				$settings = get_post_meta( $post_id, '_edd_commission_settings', true );
+				
+				// Remove dev fund rate
+				$rates      = array_map( 'trim', explode( ',', $settings['amount'] ) );
+				$rate_key   = array_search( $dev_fund_id, $recipients );
+				
+				if( false !== $rate_key ) {
+					$rates[ $rate_key ] = $amount;
+				} else {
+					$rates[] = $amount;
+				}
+
+				$settings['amount'] = implode( ',', $rates );
+
+				// Remove dev fund from recipients
 				unset( $recipients[ array_search( $dev_fund_id, $recipients ) ] );
-				$commission_settings = get_post_meta( $post_id, '_edd_commission_settings', true );
-				$commission_settings['user_id'] = implode( ',', $recipients );
-				update_post_meta( $post_id, '_edd_commission_settings', $commission_settings );
+
+				$settings['user_id'] = implode( ',', $recipients );
+
+				update_post_meta( $post_id, '_edd_commission_settings', $settings );
 
 			}
 
@@ -78,6 +220,18 @@ class CGC_Markets_FES_Dev_Fund {
 		}
 	}
 	
+	/**
+	 * Scripts
+	 *
+	 * @since 1.0
+	 *
+	 * @return void
+	 */
+	public function scripts() {	
+		wp_enqueue_script( 'jquery-ui-core' );
+		wp_enqueue_script( 'jquery-ui-slider' );
+	}
+
 	/**
 	 * Registers a setting in EDD > Settings > Misc for the development fund account ID
 	 *
